31908812a217c9eabe6b776a26cb34ac705f328d
David Nolen <david.nolen@gmail.com>
2009-09-13 16:05:06 -0400

    * index.php: use standard PHP formatting.

--- user.php
+++ user.php
@@ -1,16 +1,12 @@
 <?php
 require_once('shiftserver.php');
 
-class UserController
-{
-	public function __construct()
-  	{ 
-	
-  	}
+class UserController {
+  public function __construct() { 
+  }
 
-	public function isLoggedIn()
-	{
-		return true;
-	}
+  public function isLoggedIn() {
+    return true;
+  }
 }
 ?>
